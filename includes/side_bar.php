0a4d852edb9870aa150795bd210ec52e3b1f9780
Hussein-MSI <husseinakaz@gmail.com>
2019-06-09 17:49:25 +0300

    rearanging includes files

---
+++ includes/side_bar.php
@@ -0,0 +1,94 @@
+<!--Start body container section-->
+<div class="row container_section">
+
+<!--Start left sidebar-->
+<div class="user_details close_user_details  bring_left">
+    <div class="user_area">
+        <img class="img-thumbnail img-rounded bring_right" src="img/user.jpg">
+
+        <h1 class="h3">حسام جمال زوين</h1>
+
+        <p><a href="">بيانات المستخدم</a></p>
+
+        <p><a href="">تغيير كلمة المرور</a></p>
+
+        <p><a href="">المساعدة</a></p>
+    </div>
+    <div class="who_is_online">
+        <h3>العامليين حاليا علي النظام</h3>
+
+        <div class="employee_online">
+            <img src="img/user.jpg" class="img-circle bring_right">
+
+            <p>حسام جمال توفيق زوين</p>
+
+            <p>مركز التقنية - جامعة المنصورة</p>
+        </div>
+        <div class="employee_online">
+            <img src="img/user.jpg" class="img-circle bring_right">
+
+            <p>حسام جمال توفيق زوين</p>
+
+            <p>مركز التقنية - جامعة المنصورة</p>
+        </div>
+        <div class="employee_online">
+            <img src="img/user.jpg" class="img-circle bring_right">
+
+            <p>حسام جمال توفيق زوين</p>
+
+            <p>مركز التقنية - جامعة المنصورة</p>
+        </div>
+        <div class="employee_online">
+            <img src="img/user.jpg" class="img-circle bring_right">
+
+            <p>حسام جمال توفيق زوين</p>
+
+            <p>مركز التقنية - جامعة المنصورة</p>
+        </div>
+        <div class="employee_online">
+            <img src="img/user.jpg" class="img-circle bring_right">
+
+            <p>حسام جمال توفيق زوين</p>
+
+            <p>مركز التقنية - جامعة المنصورة</p>
+        </div>
+    </div>
+</div>
+<!--/End left sidebar-->
+
+<!--Start Side bar main menu-->
+<div class="main_sidebar bring_right">
+    <div class="main_sidebar_wrapper">
+        <form class="form-inline search_box text-center">
+            <div class="form-group">
+                <input type="search" class="form-control" placeholder="كلمة البحث">
+                <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span>
+                </button>
+            </div>
+        </form>
+
+        <ul>
+            <li><span class="glyphicon glyphicon-home"></span><a href="index.php">الصفحة الرئيسية</a></li>
+            <li><span class="glyphicon glyphicon-cog"></span><a href="options.php">بيانات الموقع</a></li>
+            <li><span class="glyphicon glyphicon-user"></span><a href="">إدارة الاعضاء</a>
+                <ul class="drop_main_menu">
+                    <li><a href="add_new_user.php">إضافة جديد</a></li>
+                    <li><a href="view_all_users.php">عرض الكل</a></li>
+                </ul>
+            </li>
+            <li><span class="glyphicon glyphicon-edit"></span><a href="">المواضييع والمقالات</a>
+                <ul class="drop_main_menu">
+                    <li><a href="add_new_topic.php">إضافة جديد</a></li>
+                    <li><a href="view_all_topics.php">عرض الكل</a></li>
+                </ul>
+            </li>
+            <li><span class="glyphicon glyphicon-picture"></span><a href="">البوم الصور</a>
+                <ul class="drop_main_menu">
+                    <li><a href="add_new_photo.php">إضافة جديد</a></li>
+                    <li><a href="view_all_photos.php">عرض الكل</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
+<!--/End side bar main menu-->
