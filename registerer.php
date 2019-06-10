78b64eb1569a265b1fa523f53eb56dc03bea0e9b
husseinakazMSI <husseinakaz@gmail.com>
2019-06-10 13:20:17 +0300

    تعديل اضافة العضو

--- registerer.php
+++ registerer.php
@@ -11,17 +11,17 @@ if(isset($_POST['registerbtn']))
   $password =  $_POST['password'];
   $cpassword =  $_POST['confirmpassword']; 
 
-  if ($password === $cpassword )
+  if ($password && $cpassword )
    {
 
     $query = "INSERT INTO `register`(`username`, `email`, `password`) VALUES ('$username','$email','$password')";
     $query_run = mysqli_query($connection, $query);
   
 
-    if($query_run)
+    if($query_run&&$username != "")
     {
       echo "saved";
-      $_SESSION["success"] = "تم اضافة العضو";
+      $_SESSION["success"] = "11تم اضافة العضو";
       header('Location: add_new_user.php');
     }
     else {
@@ -32,8 +32,20 @@ if(isset($_POST['registerbtn']))
     }
   }
     else {
-        $_SESSION['status'] = "كلة السر خاطئة او لم يتم تأكيد كلمة السر";
+        $_SESSION['status'] = "لم يتم ادخال كلمة السر وتاكيد كلمة السر!!";
         header('Location: add_new_user.php');
+    }
+    if($password != $cpassword)
+    {
+
+      echo "not saved";
+      $_SESSION["status"] = "كلة السر غير متطابقة";
+      header('Location: add_new_user.php');
+    }
+    if($username === "") {
+      echo "not saved";
+      $_SESSION["status"] = "لم لتم كتابة اسم المستخدم";
+      header('Location: add_new_user.php');
     }
   }
 
